50809d50236e2aaa79a1124d5d928bd219704e83
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-08-25 12:02:52 +0200

    Add resource file unit test

--- AndroidXml.Tests/ResReaderTests.cs
+++ AndroidXml.Tests/ResReaderTests.cs
@@ -37,5 +37,33 @@ namespace AndroidXml.Tests
                 var strings = reader.ReadResStringPool(stringPoolHeader);
             }
         }
+
+        [TestMethod]
+        [DeploymentItem("resources.arsc")]
+        public void ReadStringPoolTest2()
+        {
+            using (Stream stream = File.OpenRead("resources.arsc"))
+            using (ResReader reader = new ResReader(stream))
+            {
+                // The first chunk must be of the type res_table_type; if not,
+                // it is not an Android resources file.
+                var header = reader.ReadResChunk_header();
+                Assert.AreEqual(ResourceType.RES_TABLE_TYPE, header.Type);
+
+                // From here, we can read the resTableHeader. It indicates the number
+                // of packages that are contained in this file.
+                var tableHeader = reader.ReadResTable_header(header);
+
+                Assert.AreEqual(1u, tableHeader.PackageCount);
+
+                header = reader.ReadResChunk_header();
+                Assert.AreEqual(ResourceType.RES_STRING_POOL_TYPE, header.Type);
+
+                var stringPoolHeader = reader.ReadResStringPool_header(header);
+                var strings = reader.ReadResStringPool(stringPoolHeader);
+
+                var value = strings.StringData[0x109];
+            }
+        }
     }
 }
